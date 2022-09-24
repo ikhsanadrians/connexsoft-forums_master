6a3d5df0f059b96def08f30d28018b305c602d60
ikhsanadrians <aikhsan032@gmail.com>
2022-09-24 23:47:24 +0700

    fix eye icons change in show password btn

--- public/js/login.js
+++ public/js/login.js
@@ -19,22 +19,3 @@ $('#showpass').click(()=>{
 
 
 
-// function showBtnVisible(){
-//     let btnHidePass = $('#hidepass')
-//     let btnShowPass = $('#showpass')
-//     if(btnShowPass.classList.contains('visible')){
-//         btnShowPass.classList.remove('visible')
-//         btnShowPass.classList.add('invisible')
-//         btnHidePass.classList.remove('invisible');
-//         btnHidePass.classList.add('visible');
-//     } else {
-//         btnShowPass.classList.add('visible')
-//         btnShowPass.classList.remove('invisible')
-//         btnHidePass.classList.add('invisible');
-//         btnHidePass.classList.remove('visible');
-//     }
-
-
-//     $('#showpass').classList.add('hidden')
-// }
-
