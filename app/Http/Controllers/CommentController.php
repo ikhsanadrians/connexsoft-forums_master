5f877f1ef9eaf833fc7a1033dacda425872a2114
adriansikhsan <92657701+ikhsanadrians@users.noreply.github.com>
2022-10-08 11:19:37 +0700

    Update CommentController.php

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,7 +10,7 @@ class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * tes
      * @return \Illuminate\Http\Response
      */
     public function index()
